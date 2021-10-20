052c709f349c39098aed20eb1fb354099be1ebe6
sarahherrmann <83958698+sarahherrmann@users.noreply.github.com>
2021-10-20 16:58:30 +0200

    FindTrackableTracks.C identifies the trackable tracks and gets their kinematic

--- FindTrackableTracks.C
+++ FindTrackableTracks.C
@@ -1,5 +1,6 @@
 using MFTCluster = o2::BaseCluster<float>;
 using MFTTrack = o2::mft::TrackMFT;
+using o2::MCTrack;
 using o2::itsmft::CompClusterExt; //useful
 o2::itsmft::ChipMappingMFT mftChipMapper;
 std::vector<MFTTrack> mMFTTracks;
@@ -28,7 +29,7 @@ void FindTrackableTracks(const Char_t *kineFileName = "o2sim_Kine.root", const C
 
 
 
-  int nMFTTrackable;
+
   TFile clusterFile(clusterFileName);
   TTree* clsTree = (TTree*)clusterFile.Get("o2sim");
   std::vector<CompClusterExt> clsVec, *clsVecP = &clsVec;
@@ -63,36 +64,45 @@ void FindTrackableTracks(const Char_t *kineFileName = "o2sim_Kine.root", const C
     printf("Number of clusters detected = %d\n", nClusters);
 
     int nMFTDisksHasClusters = 0;
+    int nMFTTrackable=0;
     int trkID=0, evnID=0, srcID=0;
-    bool fake= false;
-    long long int mcLabel = 0;
-    std::vector<std::array<bool,5>> mcLabelHasClustersInMFTDisks(nClusters,{0,0,0,0,0});//taille ?
-    std::vector<long long int> mcLabelTable; //contains mcLabel and the number of occurences of this mcLabel
-    std::vector<int> nbLabelOcurrences;
-    std::vector<long long int>::iterator it; //iterator to find the index where trkID is in the mcLabelTable
     int index=-1;
+
+    double zVtx=0, pt=0, eta=0, phi=0; //Of the MCTrack
+
+    bool fake= false;
+
+    o2::MCCompLabel mcLabel;
+
+    std::vector<std::array<bool,5>> mcLabelHasClustersInMFTDisks(nClusters,{0,0,0,0,0});//taille ?
+    std::vector<long long int> mcLabelTableRaw; //contains the raw values of the mcLabel which have at least one cluster
+    std::vector<o2::MCCompLabel> mcLabelTable; //contains the mcLabel which have at least one cluster
+    std::vector<int> nbLabelOcurrences;
+    std::vector<long long int>::iterator it; //iterator to find the index where trkID is in the mcLabelTableRaw
+
 
 
       for (int icls = 0; icls < nClusters; ++icls)
       {
         auto clsEntry = icls;//? oui ?
         auto cluster = clsVec[clsEntry];
-        auto& clsLabel = (clsLabels->getLabels(clsEntry))[0];//1er label seulement ?? faire une boucle sur les labels ?
+        auto& clsLabel = (clsLabels->getLabels(clsEntry))[0];//WE SHOULD LOOP OVER LABELS
         clsLabel.get(trkID, evnID, srcID, fake);
         //printf("icls =%d , label = %llu, trackID = %d\n", icls, clsLabel.getRawValue(), trkID);
         auto clsLayer =  mftChipMapper.chip2Layer(cluster.getChipID());
         int clsMFTdiskID = clsLayer/2; //entier pour root
-        it=std::find(mcLabelTable.begin(), mcLabelTable.end(), clsLabel.getRawValue());
-          if (it != mcLabelTable.end())//trkID is already in the array mcLabelTable
+        it=std::find(mcLabelTableRaw.begin(), mcLabelTableRaw.end(), clsLabel.getRawValue());
+          if (it != mcLabelTableRaw.end())//mcLabel is already in the array mcLabelTableRaw
           {
-            index=std::distance(mcLabelTable.begin(), it);//at THIS index
+            index=std::distance(mcLabelTableRaw.begin(), it);//at THIS index
             nbLabelOcurrences[index]+=1;
             printf("#####icls =%d, mcLabel=%llu , nOcurrences =%d\n", icls, clsLabel.getRawValue(), nbLabelOcurrences[index]);
           }
-          else //trkID is not yet in mcLabelTable
+          else //mcLabel is not yet in mcLabelTableRaw
           {
-            index=mcLabelTable.size();
-            mcLabelTable.push_back(clsLabel.getRawValue()); //we add it to the table
+            index=mcLabelTableRaw.size();
+            mcLabelTableRaw.push_back(clsLabel.getRawValue()); //we add it to the tables
+            mcLabelTable.push_back(clsLabel);
             nbLabelOcurrences.push_back(1);
           }
         printf("index = %d,diskID =%d\n", index, clsMFTdiskID);
@@ -100,23 +110,43 @@ void FindTrackableTracks(const Char_t *kineFileName = "o2sim_Kine.root", const C
         mcLabelHasClustersInMFTDisks[index][clsMFTdiskID]=true;
 
       }
-      printf("Number of mcLabels stored in the mcLabelTable : %lu\n", mcLabelTable.size());
+      printf("Number of mcLabels stored in the mcLabelTableRaw : %lu\n", mcLabelTableRaw.size());
 
-      for (auto ilabel = 0 ; ilabel < mcLabelTable.size() ; ilabel++)//ilabel is the index corresponding to each mcLabel
+      for (auto ilabel = 0 ; ilabel < mcLabelTableRaw.size() ; ilabel++)//ilabel is the index corresponding to each mcLabel
       {
-
+        printf("ilabel= %d\n", ilabel);
         nMFTDisksHasClusters = 0;//has to be put in a separate IsTrackTrackable() method ?
-        //mcLabel=mcLabelTable[ilabel];
-        for(auto disk: {0,1,2,3,4}) nMFTDisksHasClusters+= int(mcLabelHasClustersInMFTDisks[mcLabel][disk]);
-        /*if(nMFTDisksHasClusters>=4)// if IsTrackTrackable(mcLabel)
-        {   //Track is trackable if has left at least 1 cluster on at least 4 different disks
-          nMFTTrackable++;
-          std::cout << "So far we have " << nMFTTrackable << " mftlabels identifying trackable MFT tracks " << std::endl;
-          MFTTrackablesEtaZ->Fill(z,eta);
-          TrackablepT->Fill(pt);
-          Trackablep->Fill(p);
-          TrackableEta->Fill(eta);
-        }*/
+        mcLabel=mcLabelTable[ilabel];//this is a real MC label, not only the raw value
+        mcLabel.get(trkID, evnID, srcID, fake);
+        kineTree->GetEntry(evnID);
+
+        if (!mcLabel.isNoise())//the Labels corresponding to noise don't give tracks in the MCTrack branch
+        {
+          MCTrack* thisTrack =  &(mcTrkVec)[trkID];
+          zVtx= eventHeader->GetZ();//YES ?? check with Antonio
+          printf("zVtx= %f\n", zVtx);
+          pt = thisTrack->GetPt();
+          eta = thisTrack->GetEta();
+
+
+          for(auto disk: {0,1,2,3,4}) nMFTDisksHasClusters+= int(mcLabelHasClustersInMFTDisks[ilabel][disk]);
+          if(nMFTDisksHasClusters>=4)// if IsTrackTrackable(mcLabel)
+          {   //Track is trackable if has left at least 1 cluster on at least 4 different disks
+            nMFTTrackable++;
+            std::cout << "So far we have " << nMFTTrackable << " mftlabels identifying trackable MFT tracks " << std::endl;
+            /*MFTTrackablesEtaZ->Fill(z,eta);
+            TrackablepT->Fill(pt);
+            Trackablep->Fill(p);
+            TrackableEta->Fill(eta);*/
+          }
+
+
+        }
+
+
+
+
+
 
 
         histNoccurencesPerMCLabel->Fill(nbLabelOcurrences[ilabel]);
