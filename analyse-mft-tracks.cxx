42f49ebc38162a57a242fac5a5938edcf4061c1e
sarahherrmann <83958698+sarahherrmann@users.noreply.github.com>
2021-11-24 15:24:06 +0100

    first working MFT task in O2Physics

--- analyse-mft-tracks.cxx
+++ analyse-mft-tracks.cxx
@@ -18,25 +18,31 @@ using Particles = aod::McParticles;
 
 struct analyseMFTTracks
 {
-  //init()?
+  int icoll = 0;
 
   HistogramRegistry registry
   {
      "registry",
      {
-       {"TracksPhiEta", "; #varphi; #eta; tracks", {HistType::kTH2F, {{600, 0, 2 * M_PI}, {21, -2.1, 2.1}}}},            //
-       {"TracksPhiEtaGen", "; #varphi; #eta; tracks", {HistType::kTH2F, {{600, 0, 2 * M_PI}, {21, -2.1, 2.1}}}},         //
+       {"TracksPhiEta", "; #varphi; #eta; tracks", {HistType::kTH2F, {{600, 0, 2 * M_PI}, {41, -5., -1.}}}},            //
+       {"TracksPhiEtaGen", "; #varphi; #eta; tracks", {HistType::kTH2F, {{600, 0, 2 * M_PI}, {21, -2.1, 2.1}}}},
+       {"Multiplicity", "alibi_nightlies/O2DPG_pp_minbias_testbeam.sh/15-11-2021-18:00 - tf13; collisionID; N_{trk}^{MFT}", {HistType::kTH1F, {{101, 0., 100.}}}, true}         //
      }                                                                                                                   //
    };
-   
-  void process(o2::aod::MFTTracks const& tracks)
+
+  void process(o2::aod::Collision const& collision, o2::aod::MFTTracks const& tracks)
     {
-      using o2::aod::MFTTracks;
+      //using o2::aod::MFTTracks;
+      //using namespace o2::aod::fwdtrack;
+      int n = 0;
 
         for (auto& track : tracks)
         {
           registry.fill(HIST("TracksPhiEta"), track.phi(), track.eta());
+          registry.fill(HIST("Multiplicity"), icoll);
+          n++;
         }
+      icoll++;
 
     }
     //end of process
@@ -44,6 +50,7 @@ struct analyseMFTTracks
 
 };
 //end of MyTask
+
 
 // struct analyseGenTracks
 // {
